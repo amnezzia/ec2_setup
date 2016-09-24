30349dc13cbf50519d635472951444f23e09f188
Misha <misha@posiba.com>
2016-09-24 13:10:30 -0700

    add matplotlib

--- 03_theano_plus.sh
+++ 03_theano_plus.sh
@@ -4,7 +4,7 @@ conda update pip
 sudo apt-get install libc-dev build-essential liblapack-dev
 conda install libgfortran gcc openblas git
 
-conda install numpy scipy pandas jupyter
+conda install numpy scipy pandas jupyter matplotlib
 
 pip install --upgrade https://github.com/Theano/Theano/archive/master.zip
 pip install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
